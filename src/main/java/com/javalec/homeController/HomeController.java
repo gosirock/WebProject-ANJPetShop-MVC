b104c9de4d3e9cea3153b9db62433d6ec15de97f
안재원 <anjaewon@anjaewon-ui-MacBookPro.local>
2023-05-26 11:16:46 +0900

    2023.05.26 패키지파일

---
+++ src/main/java/com/javalec/homeController/HomeController.java
@@ -0,0 +1,71 @@
+package com.javalec.homeController;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SemiFrrontController
+ */
+@WebServlet("*.do")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("actionDo");
+		request.setCharacterEncoding("utf-8");
+		
+		String viewPage = null;
+		
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		
+		System.out.println(uri);
+		System.out.println(conPath);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} //END
